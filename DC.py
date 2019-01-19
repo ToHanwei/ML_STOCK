28b0e798b2d9566d4b206df7755e86c049fb9091
ToHanwei <hanwei@shanghaitech.edu.cn>
2019-01-19 21:05:38 +0800

    2019-01-19_02

--- DC.py
+++ DC.py
@@ -9,14 +9,13 @@ class data_collect(object):
         ans = self.collectDATA(in_code,start_dt,end_dt)
 
     def collectDATA(self,in_code,start_dt,end_dt):
-        # 建立数据库连接，获取日线基础行情(开盘价，收盘价，最高价，最低价，成交量，成交额)
-        db = pymysql.connect(host='127.0.0.1', user='root', passwd='admin', db='stock', charset='utf8')
+        db = pymysql.connect(host='localhost', user='root', passwd='hanwei1', db='stock', charset='utf8')
         cursor = db.cursor()
         sql_done_set = "SELECT * FROM stock_all a where stock_code = '%s' and state_dt >= '%s' and state_dt <= '%s' order by state_dt asc" % (in_code, start_dt, end_dt)
         cursor.execute(sql_done_set)
         done_set = cursor.fetchall()
         if len(done_set) == 0:
-            raise Exception
+            raise ValueError("Something Error!")
         self.date_seq = []
         self.open_list = []
         self.close_list = []
@@ -34,25 +33,33 @@ class data_collect(object):
             self.amount_list.append(float(done_set[i][7]))
         cursor.close()
         db.close()
-        # 将日线行情整合为训练集(其中self.train是输入集，self.target是输出集，self.test_case是end_dt那天的单条测试输入)
         self.data_train = []
         self.data_target = []
         self.data_target_onehot = []
         self.cnt_pos = 0
         self.test_case = []
+        self.abc = []
 
-        for i in range(1,len(self.close_list)):
-            train = [self.open_list[i-1],self.close_list[i-1],self.high_list[i-1],self.low_list[i-1],self.vol_list[i-1],self.amount_list[i-1]]
+        for i in range(0,len(self.close_list)-1):
+        #for i in range(0,int(len(self.close_list)/2)):
+            train = [self.open_list[i],self.close_list[i],self.high_list[i],self.low_list[i],self.vol_list[i],self.amount_list[i]]
             self.data_train.append(np.array(train))
 
-            if self.close_list[i]/self.close_list[i-1] > 1.0:
+            if self.close_list[i+1]/self.close_list[i] > 1.0:
                 self.data_target.append(float(1.00))
                 self.data_target_onehot.append([1,0,0])
             else:
                 self.data_target.append(float(0.00))
                 self.data_target_onehot.append([0,1,0])
+        for i in range(int(len(self.close_list)/2), len(self.close_list)-1):
+            test = [self.open_list[i],self.close_list[i],self.high_list[i],self.low_list[i],self.vol_list[i],self.amount_list[i]]
+            self.test_case.append(np.array(test)) 
+            if self.close_list[i+1]/self.close_list[i] > 1.0:
+                self.abc.append(float(1.00))
+            else:
+                self.abc.append(float(0.00))
         self.cnt_pos =len([x for x in self.data_target if x == 1.00])
-        self.test_case = np.array([self.open_list[-1],self.close_list[-1],self.high_list[-1],self.low_list[-1],self.vol_list[-1],self.amount_list[-1]])
+        #self.test_case = np.array([self.open_list[-1],self.close_list[-1],self.high_list[-1],self.low_list[-1],self.vol_list[-1],self.amount_list[-1]])
         self.data_train = np.array(self.data_train)
         self.data_target = np.array(self.data_target)
         return 1
--- SVM.py
+++ SVM.py
@@ -1,16 +1,22 @@
 from sklearn import svm
 import DC
+import numpy as np
 
 if __name__ == '__main__':
-    stock = '002049.SZ'
-    dc = DC.data_collect(stock, '2017-03-01', '2018-03-01')
-    train = dc.data_train
-    target = dc.data_target
-    test_case = [dc.test_case]
-    model = svm.SVC()               # 建模
-    model.fit(train, target)        # 训练
-    ans2 = model.predict(test_case) # 预测
-    # 输出对2018-03-02的涨跌预测，1表示涨，0表示不涨。
-    print(ans2[0])
-
+	stock = '603912.SH'
+	dc = DC.data_collect(stock, '2010-01-01', '2018-12-12')
+	train = dc.data_train
+	target = dc.data_target
+	#test_case = [dc.test_case]
+	model = svm.SVC()               # 建模
+	model.fit(train, target)        # 训练
+	#for line in dc.test_case:
+	#	ans2 = model.predict([line]) # 预测
+	#	# 输出对2018-03-02的涨跌预测，1表示涨，0表示不涨。
+	#	print(ans2[0])
+	#print(type(line))
+	print(dc.abc)
+	ans2 = model.predict([np.array([0., 0., 0., 0., 0., 0.])]) # 预测
+	# 输出对2018-03-02的涨跌预测，1表示涨，0表示不涨。
+	print(ans2[0])
 
