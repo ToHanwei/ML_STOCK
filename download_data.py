15588fd7137ae901d61a7d9457d5622ff2707f68
ToHanwei <hanwei@shanghaitech.edu.cn>
2019-03-17 21:40:49 +0800

    20190317_01

--- download_data.py
+++ download_data.py
@@ -1,40 +1,66 @@
 #!coding:utf-8
 
-"""
-Download data from Tushare
-"""
-
+from pickle import load
 import tushare as ts
-import pickle
-from os.path import exists
+import pandas as pd
+import pymysql
+import requests
+from time import sleep
+from sqlalchemy import create_engine
 
-class Download():
-	def __init__(self, filename):
+class StockData(object):
+	def __init__(self, filename, database, user, port, passwd, host):
 		self.filename = filename
+		self.database = database
+		self.user = user
+		self.port = port
+		self.passwd = passwd
+		self.host = host
 		
-	def download_base(self):
-		licise = "2c93d403ebc9baab05d1754e951c52e59b14026435a644f717e58ee8"
-		pro = ts.pro_api(licise)
-		self.base = pro.stock_basic(exchange='', list_status='L')
-
-	def dump_data(self):
-		with open(self.filename, mode="wb") as outfile:
-			pickle.dump(self.base, outfile)
-		
-	def load_data(self):
-		assert exists(self.filename), self.filename + " is not exists"
+	def get_codes(self):
+		"""Obtain stock code from pkl format file"""
 		with open(self.filename, mode="rb") as infile:
-			self.data = pickle.load(infile)
+			stock_base = load(infile)
+		self.codes = stock_base['ts_code']
 
-	def filter(self):
-		self.code = self.data["ts_code"]
+	def connect_db(self):
+		"""Connect database"""
+		self.db = pymysql.connect(host="localhost",
+							 user="root",
+							 passwd="hanwei1")
+		self.cursor = self.db.cursor()
+
+	def down_data(self):
+		"""
+		Download stock daily data and save to MySQL
+		Database name is root
+		Table name is stock
+		"""
+		site = "mysql+pymysql://"+self.user+":"+self.passwd+"@"+self.host+\
+				":"+self.port+"/"+self.database
+		engine = create_engine(site)
+		pro = ts.pro_api()
+		for code in self.codes:
+			name = code.split(".")[0]
+			count = 0
+			while True:
+				try:
+					data = pro.daily(ts_code=code)
+				except requests.exceptions.ReadTimeout as e:
+					sleep(1)
+					count += 1
+					#Try too much will exist. less 100 times
+					assert count<100, name+" try too much!"
+				else:
+					print("code "+name+" has done!"); break
+			data.to_sql(name=name, con=engine, if_exists="replace", index=False)
 
 
 def main():
-	Down = Download("stocklist.pkl")
-	Down.download_base()
-	Down.dump_data()
-	
+	Stock = StockData("stocklist.pkl", "stock", "root", "3306", "hanwei1", "localhost")
+	Stock.get_codes()
+	Stock.down_data()
+
 
 if __name__ == "__main__":
 	main()
